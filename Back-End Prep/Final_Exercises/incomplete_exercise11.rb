152bdd6246de6797164377c80b46be5da6b0b476
Allan Latty <agl745@users.noreply.github.com>
2016-05-24 09:20:54 +0200

    Completed the majority of HTML/CSS module

---
+++ Back-End Prep/Final_Exercises/incomplete_exercise11.rb
@@ -0,0 +1,7 @@
+#Exercise 13
+puts contacts["Joe Smith"][:email]
+puts contacts["Sally Johnson"][:phone]
+
+#Exercise 14
+contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
+contacts = {"Joe Smith" => {}}
